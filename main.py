e618eeaa3a58d3d00959590cdc1938b117cae1ca
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-06-19 18:00:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pred import predict_risk  # Your own prediction function
+from epds import print_epds_results, epds_assessment, list_answers
+from typing import List
 
 app = FastAPI(
     title="Pregnancy Risk API"
@@ -26,3 +28,17 @@ def predict(data: InputData):
         return {"risk_level": result}
     except Exception as e:
         return {"error": str(e)}
+
+
+class EPDSInput(BaseModel):
+    responses: List[int]
+
+@app.post("/epds")
+def epds_score(data: EPDSInput):
+    try:
+        epds_score, q3, q4, q5, q10 = list_answers(data)
+        result = print_epds_results(epds_assessment(epds_score=1, q3=2, q4=1, q5=0, q10=1))
+        return result
+    except ValueError as e:
+        return {"error": str(e)}
+
