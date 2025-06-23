ae2efa037c2425d24e383346d94db22e94e2091e
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-06-22 20:32:51 -0500

    Update pred.py

--- pred.py
+++ pred.py
@@ -7,7 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 from lightgbm import LGBMClassifier
 import os
 
-FEATURE_ORDER = ['age', 'SystolicBP', 'DiastolicBP', 'BS', 'BodyTemp', 'HeartRate']
+FEATURE_ORDER = ['age', 'SystolicBP', 'DiastolicBP', 'HeartRate', 'BS', 'BodyTemp']
 
 import joblib
 
